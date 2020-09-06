be8c133500e964996ba22c59c975908a3c80baa3
Alberto <alberto.fuentesco@gmail.com>
2020-09-06 13:44:29 -0500

    Complementando template para la app posts y complementando notas de clase

--- posts/views.py
+++ posts/views.py
@@ -4,38 +4,39 @@
 from django.shortcuts import render
 
 #Utilidades
-from django.http import HttpResponse
 from datetime import datetime
 
-posts =[
+posts = [
     {
-        'name': 'Mont Blac',
-        'user': 'Melissa Tirado',
-        'timestamp':datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture':'https://picsum.photos/200/200/?image=1036',
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Melissa Tirado',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'name': 'Via Láctea',
-        'user': 'Miguel Valdez',
-        'timestamp':datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture':'https://picsum.photos/200/200/?image=903',
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Miquelsvald',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
-        'name': 'Nuevo Auditorio',
-        'user': 'Raquel Reynoso',
-        'timestamp':datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture':'https://picsum.photos/200/200/?image=1076',
-    },
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Sesitar',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
+    }
 ]
 
 # Create your views here.
 def list_posts(request):
     """Lista de posts existentes."""
-    content = []
-    for post in posts:
-            content.append("""
-                <p><strong>{name}</strong></p>
-                <p><small>{user} - <i>{timestamp}</i></small></p>
-                <figure><img src="{picture}"/></figure>
-            """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html',{'posts':posts})
